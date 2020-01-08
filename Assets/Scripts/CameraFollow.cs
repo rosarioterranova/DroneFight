818448e2ea12421ecf44fd5692ebe4b5ad846821
Rosario Terranova <rosarioterranova91@gmail.com>
2020-01-08 19:30:25 +0000

    added drone controller

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+    
+    Vector3 _targetOffset;
+
+    private void Start()
+    {
+        _targetOffset = transform.position - _target.position;
+    }
+    void Update()
+    {
+        if (_target)
+        {
+            transform.position = Vector3.Lerp(transform.position, _target.position + _targetOffset, 0.1f);
+        }
+    }
+}
--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class DroneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float speed = 10f;
 
-    // Update is called once per frame
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        if(Input.GetKey(KeyCode.E))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
     }
 }
