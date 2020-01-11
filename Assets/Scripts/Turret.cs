7beb7c0cda8cade768bd24f9b13980e1030af5dd
Rosario Terranova <rosarioterranova91@gmail.com>
2020-01-11 12:06:34 +0000

    added look at at turret

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField] GameObject top;
+
+    
+    void Update()
+    {
+        var target = FindObjectOfType<DroneController>().transform;
+        top.transform.LookAt(target);
+    }
+}
